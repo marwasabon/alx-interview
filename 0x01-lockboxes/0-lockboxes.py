245d80970946248776a69426dac8d375e935d6b2
marwasabon <marwasabon@gmail.com>
2024-08-19 13:06:55 +0300

    new task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/python3
+"""Python Script to determine if all the boxes can be opened
+   in a list of lists
+"""
+
+
+def canUnlockAll(boxes):
+    """ Function to determine if all the boxes can be opened
+    """
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
