5b087f638973811b4a7590ad147c61ad5de5ab4c
marwasabon <marwasabon@gmail.com>
2024-09-02 01:34:56 +0300

    thttt

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,39 +4,49 @@
 
 import sys  
 
+def display_stats(dic, size):  
+    """Display statistics"""  
+    print("File size: {:d}".format(size))  
+    for i in sorted(dic.keys()):  
+        if dic[i] != 0:  
+            print("{}: {:d}".format(i, dic[i]))  
 
-def display_stats(dic, size):
-    """sumary_line"""
-    print("{}: {:d}".format(i, dic[i]))
+line_count = 0  
+total_size = 0  
 
+status_codes = {  
+    "200": 0,   
+    "301": 0,   
+    "400": 0,   
+    "401": 0,   
+    "403": 0,  
+    "404": 0,   
+    "405": 0,   
+    "500": 0  
+}  
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
+try:  
+    for line in sys.stdin:  
+        if line_count != 0 and line_count % 10 == 0:  
+            display_stats(status_codes, total_size)  
 
-count = 0
-size = 0
+        stlist = line.split()  
+        line_count += 1  
 
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            display_stats(sts, size)
+        try:  
+            total_size += int(stlist[-1])  
+        except (IndexError, ValueError):  
+            pass 
 
-        stlist = line.split()
-        count += 1
+        try:  
+            if stlist[-2] in status_codes:  
+                status_codes[stlist[-2]] += 1  
+        except IndexError:  
+            pass  
 
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
+    display_stats(status_codes, total_size)  
 
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    display_stats(sts, size)
-
-
-except KeyboardInterrupt:
-    display_stats(sts, size)
-    raise
+except KeyboardInterrupt:  
+    display_stats(status_codes, total_size)  
+except BrokenPipeError:  
+    display_stats(status_codes, total_size)
