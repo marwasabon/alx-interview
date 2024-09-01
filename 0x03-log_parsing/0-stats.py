51d7f17f42c1ddb128ff7df0fbc973d217885bc4
marwasabon <marwasabon@gmail.com>
2024-09-01 18:13:30 +0300

    fixes 1

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,41 +14,41 @@ def display_stats(dic, size):
             print("{}: {:d}".format(i, dic[i]))
 
 
-line_count = 0  
-total_size = 0  
+    line_count = 0  
+    total_size = 0  
 
-status_codes = {  
-        "200": 0,   
-        "301": 0,   
-        "400": 0,   
-        "401": 0,   
-        "403": 0,  
-        "404": 0,   
-        "405": 0,   
-        "500": 0  
-}  
+    status_codes = {  
+            "200": 0,   
+            "301": 0,   
+            "400": 0,   
+            "401": 0,   
+            "403": 0,  
+            "404": 0,   
+            "405": 0,   
+            "500": 0  
+    }  
 
-try:
-    for line in sys.stdin:
-        if line_count != 0 and line_count % 10 == 0:
-            display_stats(status_codes, total_size)
+    try:
+        for line in sys.stdin:
+            if line_count != 0 and line_count % 10 == 0:
+                display_stats(status_codes, total_size)
 
-        stlist = line.split()
-        line_count += 1
+            stlist = line.split()
+            line_count += 1
 
-        try:
-            total_size += int(stlist[-1])
-        except:
-            pass
+            try:
+                total_size += int(stlist[-1])
+            except:
+                pass
 
-        try:
-            if stlist[-2] in status_codes:
-                status_codes[stlist[-2]] += 1
-        except:
-            pass
-    display_stats(status_codes, total_size)
+            try:
+                if stlist[-2] in status_codes:
+                    status_codes[stlist[-2]] += 1
+            except:
+                pass
+        display_stats(status_codes, total_size)
 
 
-except KeyboardInterrupt:
-    display_stats(status_codes, total_size)
-    raise
+    except KeyboardInterrupt:
+        display_stats(status_codes, total_size)
+        raise
